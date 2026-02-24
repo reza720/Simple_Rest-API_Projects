719f7466acc57b099ad3b23c69ca8f850b740b23
Reza Panahi <rezabumanpanahi@gmail.com>
2026-02-24 13:10:38 +0430

    auth added

--- Project1/src/models/user.js
+++ Project1/src/models/user.js
@@ -4,7 +4,9 @@ const {DataTypes}=require("sequelize");
 const User=sequelize.define("User",{
     id:{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
     name:{type:DataTypes.STRING, allowNull:false},
-    email:{type:DataTypes.STRING, allowNull:false, unique:true}
+    email:{type:DataTypes.STRING, allowNull:false, unique:true},
+    password:{type:DataTypes.STRING, allowNull:false},
+    role:{type:DataTypes.ENUM('Admin','Customer'), allowNull:false, defaultValue:"Customer"}
 },{
     timestamps:true
 });
---
+++ Project1/src/services/auth.service.js
@@ -0,0 +1,47 @@
+const dotenv=require("dotenv");
+dotenv.config();
+
+const bcrypt=require("bcrypt");
+const {User}=require("../models");
+const jwt=require("jsonwebtoken");
+const logger=require("../config/winston");
+
+// Sign Up, Create user but hash the password
+async function signup({name,email,password,role}){
+    const hashedPassword=await bcrypt.hash(password, 10);
+    const user=await User.create({
+        name,
+        email,
+        password:hashedPassword,
+        role
+    });
+    return user;
+}
+
+// Log In
+async function login({email,password}){
+    // Find the user
+    const user=await User.findOne({where:{email}});
+    if(!user){
+        logger.error(`Login failed: User with email ${email} does not exist`);
+        throw new Error(`User with email ${email} does not exist`);
+    }
+
+    // If use exist, check if the password is correct for the user
+    const isMatch= await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        logger.error(`Login failed: Invalid password`);
+        throw new Error(`Invalid password`);
+    }
+
+    // if the user is found and password is correct, user can have a token to use for each request
+    const token=jwt.sign(
+        {id:user.id, name:user.name, email:user.email, role:user.role},
+        process.env.JWT_SECRET || "secretKey",
+        {expiresIn:"1h"}
+    );
+    logger.info(`User with email ${email} successfully logged in`);
+    return {token, user:{id:user.id, name:user.name, role:user.role}};
+}
+
+module.exports={signup, login};
