88c59887fac5de9a0c53edf03abb0482cf4b7940
Reza Panahi <rezabumanpanahi@gmail.com>
2026-02-25 12:19:11 +0430

    services updated

--- Project1/src/services/auth.service.js
+++ Project1/src/services/auth.service.js
@@ -7,45 +7,46 @@ const jwt=require("jsonwebtoken");
 const logger=require("../config/winston");
 const notFoundError=require("../utils/notFoundError");
 
-// Sign Up, Create user but hash the password
-async function signup({name,email,password,role}){
-    const hashedPassword=await bcrypt.hash(password, 10);
-    const user=await User.create({
-        name,
-        email,
-        password:hashedPassword,
-        role
-    });
-    logger.info(`User with name ${name} and email ${email} signed up`);
-    return user;
-}
-
-// Log In
-async function login({email,password}){
-    // Find the user
-    const user=await User.findOne({where:{email}});
-    if(!user){
-        logger.warn(`Login failed: User with email ${email} does not exist`);
-        notFoundError(`User with email ${email}`);
+class AuthService{
+    // Sign Up, Create user but hash the password
+    async signup({name,email,password,role}){
+        const hashedPassword=await bcrypt.hash(password, 10);
+        const user=await User.create({
+            name,
+            email,
+            password:hashedPassword,
+            role
+        });
+        logger.info(`User with name ${name} and email ${email} signed up`);
+        return user;
     }
+    // Log In
+    async login({email,password}){
+        // Find the user
+        const user=await User.findOne({where:{email}});
+        if(!user){
+            logger.warn(`Login failed: User with email ${email} does not exist`);
+            notFoundError(`User with email ${email}`);
+        }
 
-    // If use exist, check if the password is correct for the user
-    const isMatch= await bcrypt.compare(password, user.password);
-    if(!isMatch){
-        logger.warn(`Login failed for ${email}: Invalid password`);
-        const err=new Error(`Invalid password`);
-        err.statusCode=401;
-        throw err;
+        // If use exist, check if the password is correct for the user
+        const isMatch= await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            logger.warn(`Login failed for ${email}: Invalid password`);
+            const err=new Error(`Invalid password`);
+            err.statusCode=401;
+            throw err;
+        }
+
+        // if the user is found and password is correct, user can have a token to use for each request
+        const token=jwt.sign(
+            {id:user.id, name:user.name, email:user.email, role:user.role},
+            process.env.JWT_SECRET || "secretKey",
+            {expiresIn:"1h"}
+        );
+        logger.info(`User with email ${email} successfully logged in`);
+        return {token, user:{id:user.id, name:user.name, role:user.role}};
     }
-
-    // if the user is found and password is correct, user can have a token to use for each request
-    const token=jwt.sign(
-        {id:user.id, name:user.name, email:user.email, role:user.role},
-        process.env.JWT_SECRET || "secretKey",
-        {expiresIn:"1h"}
-    );
-    logger.info(`User with email ${email} successfully logged in`);
-    return {token, user:{id:user.id, name:user.name, role:user.role}};
 }
+module.exports=new AuthService();
 
-module.exports={signup, login};
---
+++ Project1/src/services/index.js
@@ -0,0 +1,5 @@
+const AuthService=require("./auth.service");
+const UserService=require("./user.service");
+const ItemService=require("./item.service");
+
+module.exports={AuthService,UserService,ItemService};
