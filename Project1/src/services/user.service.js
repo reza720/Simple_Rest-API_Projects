17397af51bb3d70d65b7bc541768b119032fa68d
Reza Panahi <rezabumanpanahi@gmail.com>
2026-02-25 12:02:55 +0430

    user services added

---
+++ Project1/src/services/user.service.js
@@ -0,0 +1,35 @@
+const {User}=require("../models");
+const logger=require("../config/winston");
+const notFoundError=require("../utils/notFoundError");
+
+class UserService{
+    // Read Users, Only Admin use it
+    async getUsers(options={}){
+        const users=await User.findAll({where:options});
+        if(users.length===0){
+            logger.warn(`No users found with filters of ${JSON.stringify(options)}`);
+            notFoundError("users");
+        }
+        return users;
+    }
+
+    // Update User, Both Admin and Customer user it, but for onyl themselves
+    async updateUser(id,data){
+        const user=await User.findByPk(id);
+        if(!user)notFoundError("user");
+        await user.update(data);
+        logger.info(`User with ID: ${id} Updated`);
+        return user;
+    }
+
+    // Delete User, Both Admin and Customer user it, admin can use for both self and customers
+    async deleteUser(id){
+        const user=await User.findByPk(id);
+        if(!user)notFoundError("user");
+        await user.destroy();
+        logger.info(`User with ID: ${id} Deleted`);
+        return {message:"Deleted"};
+    }
+};
+
+module.exports=new UserService();
