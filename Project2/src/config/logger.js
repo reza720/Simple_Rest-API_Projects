5f8624edc38782c3f9b8295081cefbf8eac59cdc
Reza Panahi <rezabumanpanahi@gmail.com>
2026-03-08 10:13:11 +0430

    logger config added

---
+++ Project2/src/config/logger.js
@@ -0,0 +1,35 @@
+const winston=require("winston");
+const fs=require("fs");
+const path=require("path");
+
+const logs=path.join(process.cwd(),"logs");
+fs.mkdirSync(logs,{recursive:true});
+
+const logger=winston.createLogger({
+  level: "info",
+  format:winston.format.combine(
+    winston.format.timestamp({format:"YYYY-MM-DD HH:mm:ss"}),
+    winston.format.errors({stack:true}),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports:[
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
+
+    new winston.transports.File({
+      filename:path.join(logs,"allLogs.log")
+    }),
+
+    new winston.transports.File({
+      filename:path.join(logs,"errors.log"),
+      level:"error"
+    })
+  ]
+});
+
+module.exports=logger;
