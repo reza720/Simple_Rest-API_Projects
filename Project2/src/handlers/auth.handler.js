8e4840e00d3415017b05100a973f262e57c21d56
Reza Panahi <rezabumanpanahi@gmail.com>
2026-03-09 13:20:16 +0430

    user handler added

--- Project2/src/handlers/auth.handler.js
+++ Project2/src/handlers/auth.handler.js
@@ -4,7 +4,7 @@ class AuthHandler{
     static async signup(req,res,next){
         try{
             const user=await AuthService.signup(req.body);
-            res.status(201).json({success:true, message:"successfully Signed Up",data:{
+            res.status(201).json({success:true, message:"Signed Up",data:{
                 email:user.email,
                 fullName:user.fullName
             }});
@@ -17,7 +17,7 @@ class AuthHandler{
     static async login(req,res,next){
         try{
             const result= await AuthService.login(req.body);
-            res.status(200).json({success:true, message:"User Logged In", data:result});
+            res.status(200).json({success:true, message:"Logged In", data:result});
         }
         catch(err){
             next(err);
---
+++ Project2/src/handlers/user.handler.js
@@ -0,0 +1,35 @@
+const {UserService}=require("../services");
+
+class UserHandler{
+    static async getUsers(req,res,next){
+        try{
+            const users=await UserService.getUsers();
+            res.status(200).json({success:true, data:users});
+        }
+        catch(err){
+            next(err);
+        }
+    }
+
+    static async updateUser(req,res,next){
+        try{
+            const user=await UserService.updateUser(req.params.id, req.body);
+            res.status(200).json({success:true, data:user})
+        }
+        catch(err){
+            next(err);
+        }
+    }
+
+    static async deleteUser(req,res,next){
+        try{
+            await UserService.deleteUser(req.params.id);
+            res.status(204).send();
+        }
+        catch(err){
+            next(err);
+        }
+    }
+}
+
+module.exports=UserHandler;
