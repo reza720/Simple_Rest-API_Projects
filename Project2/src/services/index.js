0eade81c363d85f418f5cca4e57f6a0ca7b25a96
Reza Panahi <rezabumanpanahi@gmail.com>
2026-03-09 11:24:44 +0430

    index added to service

---
+++ Project2/src/services/index.js
@@ -0,0 +1,5 @@
+const AuthService=require("./auth.service");
+const UserService=require("./user.service");
+const PropertyService=require("./property.service");
+
+module.exports={AuthService, UserService, PropertyService};
