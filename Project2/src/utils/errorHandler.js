6fa1d95105ebd43962be4dafe243c10470063dc9
Reza Panahi <rezabumanpanahi@gmail.com>
2026-03-10 16:01:37 +0430

    errorhandler for validation added

---
+++ Project2/src/utils/errorHandler.js
@@ -0,0 +1,14 @@
+const { validationResult } = require("express-validator");
+
+const errorHandler = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const err = new Error("Validation Failed");
+        err.statusCode = 400;
+        err.data = errors.array(); 
+        return next(err);
+    }
+    next();
+};
+
+module.exports = errorHandler;
