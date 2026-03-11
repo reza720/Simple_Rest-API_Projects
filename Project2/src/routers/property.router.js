d33650990f99a858d7528508436839e52574018f
Reza Panahi <rezabumanpanahi@gmail.com>
2026-03-11 11:20:44 +0430

    Property router added

--- Project2/src/routers/property.router.js
+++ Project2/src/routers/property.router.js
@@ -1,3 +1,39 @@
+const express=require("express");
+const router=express.Router();
+
 const {PropertyHandler}=require("../handlers");
 const validate=require("../utils/validate");
-const 
+const PropertyRules=require("../middlewares/validations/property.validation");
+const AuthenticateToken=require("../middlewares/authenticateToken");
+
+router.post(
+    "/", 
+    AuthenticateToken,
+    PropertyRules,
+    validate,
+    PropertyHandler.createProperty
+);
+
+router.get(
+    "/",
+    AuthenticateToken,
+    PropertyHandler.getProperties
+);
+
+router.put(
+    "/:id",
+    AuthenticateToken,
+    PropertyRules,
+    validate,
+    PropertyHandler.updateProperty
+);
+
+router.delete(
+    "/:id",
+    AuthenticateToken,
+    PropertyHandler.deleteProperty
+);
+
+module.exports=router;
+
+
